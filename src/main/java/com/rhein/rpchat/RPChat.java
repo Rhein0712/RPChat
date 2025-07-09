8cbaf77f0f8267d2dc6aedb53927a3e534619bbe
Rhein0712 <165314392+Rhein0712@users.noreply.github.com>
2025-07-09 14:56:32 -0400

    Whisper version 1

--- src/main/java/com/rhein/rpchat/RPChat.java
+++ src/main/java/com/rhein/rpchat/RPChat.java
@@ -4,12 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.rhein.rpchat.MeCommand;
 
+import com.rhein.rpchat.WhisperCommand;
+
 public class RPChat extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("RPChat enabled.");
         getCommand("me").setExecutor(new MeCommand());
+        getCommand("whisper").setExecutor(new WhisperCommand());
         // We'll register commands and events here later
     }
 
