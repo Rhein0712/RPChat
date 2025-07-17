605df7fc4552aa81c28b7eb1da8c8e78903700e4
Rhein0712 <165314392+Rhein0712@users.noreply.github.com>
2025-07-16 23:55:51 -0400

    Clarifications

--- src/main/java/com/rhein/rpchat/commands/RPChatHelpCommand.java
+++ src/main/java/com/rhein/rpchat/commands/RPChatHelpCommand.java
@@ -11,12 +11,12 @@ public class RPChatHelpCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         sender.sendMessage(ChatColor.GOLD + "----- RPChat Help -----");
-        sender.sendMessage(ChatColor.GREEN + "/rp" + ChatColor.GRAY + " - Toggle RP chat mode");
+        sender.sendMessage(ChatColor.GREEN + "/rp" + ChatColor.GRAY + " - Toggle RP chat mode, which sends messages IRP (within a 20-block radius)");
         sender.sendMessage(ChatColor.GREEN + "/ooc" + ChatColor.GRAY + " - Switch to global OOC chat");
-        sender.sendMessage(ChatColor.GREEN + "/whisper <msg>" + ChatColor.GRAY + " - Whisper to nearby players");
-        sender.sendMessage(ChatColor.GREEN + "/yell <msg>" + ChatColor.GRAY + " - Yell to farther players");
-        sender.sendMessage(ChatColor.GREEN + "/looc <msg>" + ChatColor.GRAY + " - Local OOC message");
-        sender.sendMessage(ChatColor.GREEN + "/me <emote>" + ChatColor.GRAY + " - Send an emote/action");
+        sender.sendMessage(ChatColor.GREEN + "/whisper <msg>" + ChatColor.GRAY + " - Whisper to nearby players (within a 5-block radius)");
+        sender.sendMessage(ChatColor.GREEN + "/yell <msg>" + ChatColor.GRAY + " - Yell to farther players IRP (within a 50-block radius)");
+        sender.sendMessage(ChatColor.GREEN + "/looc <msg>" + ChatColor.GRAY + " - Local OOC message (within a 20-block radius)");
+        sender.sendMessage(ChatColor.GREEN + "/me <emote>" + ChatColor.GRAY + " - Send an emote/action (within a 20-block radius)");
         sender.sendMessage(ChatColor.GREEN + "/staff" + ChatColor.GRAY + " - Toggle staff chat mode (requires access)");
         sender.sendMessage(ChatColor.GREEN + "/staffadd <player>" + ChatColor.GRAY + " - Give staff access " + ChatColor.RED + "(OP only)");
         sender.sendMessage(ChatColor.GREEN + "/staffremove <player>" + ChatColor.GRAY + " - Remove staff access " + ChatColor.RED + "(OP only)");
